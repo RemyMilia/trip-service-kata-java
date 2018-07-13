15c8c26c03dc63ef4214940d4d249eacb310297b
Rémy Milia <milia.remy@gmail.com>
2018-07-13 17:52:10 +0200

    test second use case: no trips retrieved for no user friend

--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,22 +1,42 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.assertj.core.api.Assertions;
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.junit.Test;
 
+import java.util.Collections;
+import java.util.List;
+
 public class TripServiceTest {
+
+    private static final User NO_USER = null;
+    private static final User UNKNOWN_USER = new User();
 
     @Test(expected = UserNotLoggedInException.class)
     public void should_throw_an_exception_if_current_user_is_not_logged_in() {
-        TripServiceChild tripService = new TripServiceChild();
-        tripService.getTripsByUser(null);
+        TripServiceChild tripService = new TripServiceChild(NO_USER);
+        tripService.getTripsByUser(NO_USER);
+    }
+
+    @Test
+    public void should_return_no_trips_for_an_unknown_user() {
+        TripServiceChild tripService = new TripServiceChild(UNKNOWN_USER);
+        List<Trip> trips = tripService.getTripsByUser(UNKNOWN_USER);
+        Assertions.assertThat(trips).isEqualTo(Collections.EMPTY_LIST);
     }
 
     private class TripServiceChild extends TripService {
 
+        private User loggedUser;
+
+        protected TripServiceChild(User user) {
+            this.loggedUser = user;
+        }
+
         @Override
         protected User getLoggedUser() {
-            return null;
+            return this.loggedUser;
         }
 
     }
