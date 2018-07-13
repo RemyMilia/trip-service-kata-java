cc56aecd374c0941efe948b857218a396efdab22
Rémy Milia <milia.remy@gmail.com>
2018-07-13 16:03:45 +0200

    add assertJ support, fake test

--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,14 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class TripServiceTest {
+
+    @Test
+    public void explore() {
+        assertThat(true).isEqualTo(true);
+    }
 
 }
