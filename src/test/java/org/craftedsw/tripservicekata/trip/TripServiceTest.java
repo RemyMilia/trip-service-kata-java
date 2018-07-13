326d1d82826faca7ebba38d8bf02c419ac02f077
Rémy Milia <milia.remy@gmail.com>
2018-07-13 16:53:33 +0200

    Fix first test

--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,14 +1,24 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
 import org.junit.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class TripServiceTest {
 
-    @Test
-    public void explore() {
-        assertThat(true).isEqualTo(true);
+    @Test(expected = UserNotLoggedInException.class)
+    public void should_throw_an_exception_if_current_user_is_not_logged_in() {
+        TripServiceChild tripService = new TripServiceChild();
+        tripService.getTripsByUser(null);
+    }
+
+    private class TripServiceChild extends TripService {
+
+        @Override
+        protected User getLoggedUser() {
+            return null;
+        }
+
     }
 
 }
