6a305cf11f1e625f5185793b2ad4dfd530976234
Rémy Milia <milia.remy@gmail.com>
2018-07-17 10:59:20 +0200

    refacto TripService getTripsByUser()

--- src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -4,29 +4,21 @@ import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TripService {
 
-    public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-        List<Trip> tripList = new ArrayList<Trip>();
-        User loggedUser = getLoggedUser();
-        boolean isFriend = false;
-        if (loggedUser != null) {
-            for (User friend : user.getFriends()) {
-                if (friend.equals(loggedUser)) {
-                    isFriend = true;
-                    break;
-                }
-            }
-            if (isFriend) {
-                tripList = tripsByUser(user);
-            }
-            return tripList;
-        } else {
+    protected List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
+        if (getLoggedUser() == null) {
             throw new UserNotLoggedInException();
         }
+        return user.isFriendWith(getLoggedUser()) ? tripsByUser(user) : noTrips();
+
+    }
+
+    private List noTrips() {
+        return Collections.EMPTY_LIST;
     }
 
     protected List<Trip> tripsByUser(User user) {
